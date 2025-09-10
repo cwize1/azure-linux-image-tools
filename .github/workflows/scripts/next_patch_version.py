edde801b61e36d6bbae28ffc445091c9daafc593
Chris Gunn <chrisgun@microsoft.com>
2025-09-10 12:37:32 -0700

    WIP: Test pre-fork release

--- .github/workflows/scripts/next_patch_version.py
+++ .github/workflows/scripts/next_patch_version.py
@@ -15,11 +15,25 @@ def main():
 
     mostRecentVersion = versions[0]
 
-    if mostRecentVersion[0] != makefileVersion[0] or mostRecentVersion[1] != makefileVersion[1]:
-        print(f"Makefile and most recent major/minor versions don't match: {makefileVersion} vs. {mostRecentVersion}", file=sys.stderr)
+    makefileVersionMgrMin = makefileVersion[0:2]
+    mostRecentVersionMgrMin = mostRecentVersion[0:2]
+
+    if makefileVersionMgrMin < mostRecentVersionMgrMin:
+        print(f"Makefile major/minor version is less than most recent major/minor version: "+
+            f"{makefileVersion} vs. {mostRecentVersion}",
+            file=sys.stderr)
         exit(3)
 
-    newPatchVersion = (mostRecentVersion[0], mostRecentVersion[1], mostRecentVersion[2]+1)
+    elif makefileVersionMgrMin == mostRecentVersionMgrMin:
+        # A previous version tag for the current major/minor version exists.
+        # So, increment the patch version.
+        newPatchVersion = (mostRecentVersion[0], mostRecentVersion[1], mostRecentVersion[2]+1)
+
+    else:
+        # A version tag doesn't doesn't exist yet for the current major/minor version.
+        # So, start from patch 0.
+        newPatchVersion = makefileVersion
+
     print(f"{newPatchVersion[0]}.{newPatchVersion[1]}.{newPatchVersion[2]}")
 
 if __name__ == "__main__":
