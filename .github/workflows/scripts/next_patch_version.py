44d16f0c5886de8458e5a7836d0cf24a3ec0f68e
Chris Gunn <chrisgun@microsoft.com>
2025-09-10 13:04:01 -0700

    TEST4

--- .github/workflows/scripts/next_patch_version.py
+++ .github/workflows/scripts/next_patch_version.py
@@ -9,22 +9,21 @@ def main():
     makefileVersion = getMakefileVersion()
 
     versions = getVersionTags(merged=True)
-    if len(versions) <= 0:
-        print(f"No previous tags found", file=sys.stderr)
-        exit(2)
 
-    mostRecentVersion = versions[0]
+    mostRecentVersion = None
+    if len(versions) > 0:
+        mostRecentVersion = versions[0]
 
     makefileVersionMgrMin = makefileVersion[0:2]
     mostRecentVersionMgrMin = mostRecentVersion[0:2]
 
-    if makefileVersionMgrMin < mostRecentVersionMgrMin:
+    if mostRecentVersion is not None and makefileVersionMgrMin < mostRecentVersionMgrMin:
         print(f"Makefile major/minor version is less than most recent major/minor version: "+
             f"{makefileVersion} vs. {mostRecentVersion}",
             file=sys.stderr)
         exit(3)
 
-    elif makefileVersionMgrMin == mostRecentVersionMgrMin:
+    elif mostRecentVersion is not None and makefileVersionMgrMin == mostRecentVersionMgrMin:
         # A previous version tag for the current major/minor version exists.
         # So, increment the patch version.
         newPatchVersion = (mostRecentVersion[0], mostRecentVersion[1], mostRecentVersion[2]+1)
